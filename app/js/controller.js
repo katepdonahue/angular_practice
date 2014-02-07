dbf9d7e816d77102d8480135d7473ede0396a972
Kate <katepdonahue@gmail.com>
2014-02-07 17:13:17 -0500

    Render json from external file

--- app/js/controller.js
+++ app/js/controller.js
@@ -2,39 +2,8 @@
 
 var handshakeApp = angular.module('handshakeApp', []);
 
-handshakeApp.controller('HandshakeListCtrl', function($scope){
-  $scope.handshakes = [
-    { "id": 1,
-      "name": "catgrab",
-      "attributes": "Firm and confident on one hand, sleepy on the other.",
-      "context": "Cat has it's paw out."},
-    { "id": 1,
-      "name": "community",
-      "attributes": "Ambidextrous coordination.",
-      "context": "When you see something cool that you like."},
-    { "id": 1,
-      "name": "fingerguns",
-      "attributes": "Handtickle, fingerguns.",
-      "context": "Agreement, conspiracy."},
-    { "id": 1,
-      "name": "gentlemancat",
-      "attributes": "Friendly, cordial.",
-      "context": "In the presence of a distinguished cat."},
-    { "id": 1,
-      "name": "heyarnold",
-      "attributes": "Thumb-high-five.",
-      "context": "When you see your best friend."},
-    { "id": 1,
-      "name": "jazzhandshake",
-      "attributes": "Lean back.",
-      "context": "Things are going your way."},
-    { "id": 1,
-    "name": "owl",
-    "attributes": "talons.",
-    "context": "When you meet an owl for the first time."},
-    { "id": 1,
-    "name": "stillwalking",
-    "attributes": "Complicated finger weaving, never stop walking.",
-    "context": "You just won."}
-  ];
+handshakeApp.controller('HandshakeListCtrl', function($scope, $http){
+  $http.get('handshakes/handshakes.json').success(function(data) {
+    $scope.handshakes = data;
+  });
 });
